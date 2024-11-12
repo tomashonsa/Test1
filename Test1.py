7359cac8f6493cc0616c61e639719a814ec649ce
Tomáš Honsa <Tomas.Honsa@student.souepl.cz>
2024-11-12 13:12:44 +0100

    n

--- Test1.py
+++ Test1.py
@@ -1,18 +1,18 @@
 while True:
     vyber = input("Vyber si jaká je neznáma (Proud -> I, Napeti -> U, Odpor -> R) ") # proud-amper napeti-volt odpor-Ω
 
-    if vyber == "proud":
+    if vyber == "I":
         Napeti = input("Jaký je napětí?")
         Odpor = input("Jaký je Odpor?")
         Napeti =int(Napeti)
         Odpor =int(Odpor)
-        if Odpor == 0:
-            print("nelze dělit nulou")
+        if Odpor <= 0:
+            print("nelze dělit nulou nebo minusem")
         else:
             print("R=", Odpor,"Ω"," " "U=",Napeti,"V"," ","I=?")
             print("I=", Napeti,"/",Odpor)
             print("I=", Napeti/Odpor,"A")
-    elif vyber == "napeti":
+    elif vyber == "U":
         Proud = input("Jaký je Proud?")
         Odpor = input("Jaký je odpor?")
         Proud = int(Proud)
@@ -20,13 +20,13 @@ while True:
         print("R=", Odpor,"Ω"," " "I=",Proud,"A"," ","U=?")
         print("U=", Proud,"*",Odpor)
         print("U=", Proud*Odpor,"V")
-    elif vyber == "odpor":
+    elif vyber == "R":
         Napeti = input("Jaký je napětí?")
         Proud = input("Jaký je Proud?")
         Napeti =int(Napeti)
         Proud =int(Proud)
-        if Proud == 0:
-            print("nelze dělit nulou")
+        if Proud <= 0:
+            print("nelze dělit nulou nebo minusem")
         else:
             print("U=",Napeti,"V"," ""I=", Proud,"A"," ","R=?")
             print("R=", Napeti,"/",Proud)
